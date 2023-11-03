a56e82c5e99165f86b7bd8a218a011d9bad0c046
dfguerrerom <dfgm2006@gmail.com>
2023-11-03 10:17:01 +0000

    auto add dirs

--- gee_scripts/__init__.py
+++ gee_scripts/__init__.py
@@ -1,12 +1,1 @@
-from pathlib import Path
-
-
-explanatory_path = Path("data/11_explanatory_composites")
-output_estimation_path = Path("data/12_estimated_gwl")
-model_path = Path("data/10_models/")
-
-
-# Create them if they don't exist
-explanatory_path.mkdir(exist_ok=True)
-output_estimation_path.mkdir(exist_ok=True)
-model_path.mkdir(exist_ok=True)
+from .directories import *
